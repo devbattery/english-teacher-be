3eb118ad17cc8dc00fe7303ed06dc47a74dcf9ff
devbattery <devbattery@outlook.com>
2025-05-17 18:22:30 +0900

    Initial Commit

---
+++ src/main/java/com/devbattery/englishteacher/EnglishTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.devbattery.englishteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishTeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishTeacherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devbattery/englishteacher/EnglishTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbattery.englishteacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishTeacherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
