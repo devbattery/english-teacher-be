c786a4b9261cdcff653e0428d54d8993138e5fb8
devbattery <devbattery@outlook.com>
2025-06-02 21:23:32 +0900

    Feat: Redis에 Refresh Token를 저장하도록 구현

---
+++ src/main/java/com/devbattery/englishteacher/auth/application/service/RefreshTokenService.java
@@ -0,0 +1,40 @@
+package com.devbattery.englishteacher.auth.application.service;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Value("${jwt.refresh-token-expire-time}")
+    private long refreshTokenExpireTime;
+
+    private static final String REFRESH_TOKEN_PREFIX = "RT:";
+
+    public void saveToken(String email, String refreshToken) {
+        String key = REFRESH_TOKEN_PREFIX + email;
+        redisTemplate.opsForValue().set(
+                key,
+                refreshToken,
+                refreshTokenExpireTime,
+                TimeUnit.MILLISECONDS
+        );
+    }
+
+    public String getToken(String email) {
+        String key = REFRESH_TOKEN_PREFIX + email;
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteToken(String email) {
+        String key = REFRESH_TOKEN_PREFIX + email;
+        redisTemplate.delete(key);
+    }
+
+}
---
+++ src/main/java/com/devbattery/englishteacher/common/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package com.devbattery.englishteacher.common.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperties.getHost());
+        config.setPort(redisProperties.getPort());
+        config.setPassword(redisProperties.getPassword());
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(config);
+        log.info("LettuceConnectionFactory 생성 완료.");
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
