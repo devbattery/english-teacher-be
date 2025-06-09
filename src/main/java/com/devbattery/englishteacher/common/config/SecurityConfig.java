45523829295edbeae8898a4cea9fde2b404b3200
devbattery <devbattery@outlook.com>
2025-06-09 18:13:33 +0900

    Feat: cors 설정 추가
    임시로 하드 코딩

--- src/main/java/com/devbattery/englishteacher/common/config/SecurityConfig.java
+++ src/main/java/com/devbattery/englishteacher/common/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.devbattery.englishteacher.common.config;
 import com.devbattery.englishteacher.auth.application.service.CustomOAuth2UserService;
 import com.devbattery.englishteacher.common.filter.JwtAuthFilter;
 import com.devbattery.englishteacher.common.handler.OAuth2LoginSuccessHandler;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -35,6 +40,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/auth/token").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/auth/refresh").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/auth/logout").permitAll()
                         .anyRequest().authenticated()
                 ).oauth2Login(
                         oauth2 -> oauth2
@@ -44,6 +51,25 @@ public class SecurityConfig {
                 ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // TODO: (임시)
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        // 허용할 HTTP 메서드를 지정합니다.
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // 허용할 HTTP 헤더를 지정합니다.
+        configuration.setAllowedHeaders(List.of("*"));
+        // 자격 증명(쿠키, 인증 헤더 등)을 허용합니다.
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 모든 경로에 대해 위에서 정의한 CORS 설정을 적용합니다.
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
